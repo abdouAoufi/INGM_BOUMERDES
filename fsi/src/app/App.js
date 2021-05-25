e385d5a34aa170f36c8ad0afb118df4fd6e47b8a
Abderahmane Aoufi <abdouou7@gmail.com>
2021-05-25 19:01:06 +0100

    set up project

---
+++ fsi/src/app/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <p className="">This is a simple web page !!</p>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
