8f6c7c5d7e83fdf726e282e9781fe59df8c1b41a
Abderahmane Aoufi <abdouou7@gmail.com>
2021-06-10 19:50:14 +0100

    begin developping calendar page

--- fsi/src/components/HeaderHome/HeaderHome.js
+++ fsi/src/components/HeaderHome/HeaderHome.js
@@ -3,7 +3,7 @@ import React from "react";
 function HeaderHome({ title, text, image }) {
   return (
     <div>
-      <div className="h-56 mx-auto  lg:shadow-lg  flex px-4 py-3 justify-between items-center  my-4 rounded-xl">
+      <div className="h-56 mx-auto bg-bgcolor lg:shadow-lg  flex px-4 py-3 justify-between items-center  my-4 rounded-xl">
         <div>
           <h1 className="text-2xl font-bold mb-2">{title}</h1>
           <p className="text-base">{text}</p>
