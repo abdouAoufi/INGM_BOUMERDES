36a4af510fd85a8f4667e9988404cc1241d0c3bc
Abderahmane Aoufi <abdouou7@gmail.com>
2021-06-18 12:07:12 +0100

    move wel

--- fsi/src/components/Calendar/Calendar.js
+++ fsi/src/components/Calendar/Calendar.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Calendar.css";
 import Events from "./Events/Events";
 import Header from "../HeaderHome/HeaderHome";
-import * as assetes from "../../assets/assetes"
+import * as assetes from "../../assets/assetes";
 function Calendar() {
   return (
     <div>
@@ -14,11 +14,12 @@ function Calendar() {
         />
       </div>
       <div className="mobile-wrapper">
-        
-
         <section className="today-box" id="today-box">
           <span className="breadcrumb">Aujourduit</span>
           <h3 className="date-title">Mercredi 03 - May</h3>
+          <button class="bg-yellow-400 mt-4 px-5 py-3 text-sm shadow-sm font-bold tracking-wider  text-white  rounded-full  hover:bg-yellow-500">
+            Telecharger l'empoloi
+          </button>
         </section>
         <Events />
       </div>
--- fsi/src/components/Calendar/Events/Events.js
+++ fsi/src/components/Calendar/Events/Events.js
@@ -1,14 +1,68 @@
 import React from "react";
-import "../Calendar.css"
+import "../Calendar.css";
 
 function Events() {
+  const events = [
+    {
+      time: "8:00",
+      duration: "1h:30min",
+      descreption: "Systeme embarquee",
+      active: false,
+    },
+    {
+      time: "9:40",
+      duration: "1h:30min",
+      descreption: "System non linear",
+      active: false,
+    },
+    {
+      time: "11:00",
+      duration: "1h:00min",
+      descreption: "Electronique appliquee",
+      active: true,
+    },
+    {
+      time: "12:30",
+      duration: "1h:50min",
+      descreption: "Reseaux informatique",
+      active: false,
+    },
+    {
+      time: "13:40",
+      duration: "1h:00min",
+      descreption: "Mathematique",
+      active: false,
+    },
+    {
+      time: "15:00",
+      duration: "1h:30min",
+      descreption: "Mecanic",
+      active: false,
+    },
+  ];
   return (
     <div>
       <section className="upcoming-events">
         <div className="container">
           <h3 className="misajour">Total horaire : 4h </h3>
           <div className="events-wrapper">
-            <div className="event">
+            {events.map((event) => {
+              return (
+                <div className={`event ${event.active ? "active" : null}`}>
+                  <i className="ion ion-ios-flame hot" />
+                  <h4
+                    className={`event__point ${
+                      event.active ? "active-time" : null
+                    }`}
+                  >
+                    {event.time}
+                  </h4>
+                  <span className="event__duration">{event.duration}</span>
+                  <p className="event__description">{event.descreption}</p>
+                </div>
+              );
+            })}
+            {/* <div className="event">
               <i className="ion ion-ios-flame hot" />
               <h4 className="event__point">08:00 am</h4>
               <span className="event__duration">in 30 minutes.</span>
@@ -36,7 +90,7 @@ function Events() {
               <h4 className="event__point">01:15 pm</h4>
               <span className="event__duration">in 20 minutes.</span>
               <p className="event__description">API</p>
-            </div>
+            </div> */}
           </div>
         </div>
       </section>
